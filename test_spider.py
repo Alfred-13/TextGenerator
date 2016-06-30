40b10be71c2b583c8cbe0f0512af1b09c1b96777
dcclogin <dccrumia@gmail.com>
2016-06-30 17:38:36 +0800

    cd TextGenerator/!

--- test_spider.py
+++ test_spider.py
@@ -1,3 +1,4 @@
+import requests
 from urllib2 import *
 from bs4 import BeautifulSoup
 
@@ -7,6 +8,7 @@ url1 = 'http://www.ieee-security.org/TC/SP2016/program.html'
 url2 = 'http://www.ieee-security.org/TC/SP2016/program-papers.html'
 # program-posters (pdfs in this url)
 url3 = 'http://www.ieee-security.org/TC/SP2016/program-posters.html'
+
 
 def get_titles(url):
     """ return a dict containing all titles in one page
@@ -37,3 +39,37 @@ def get_intros(url):
 
 #intros = get_intros(url)
 #print intros
+
+def download_pdf(url):  
+    """ download the pdf to local given the exact url
+    """
+    file_name = url.split('/')[-1]
+    r = requests.get(url, stream=True)
+    with open(file_name, 'wb') as dst_file:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                dst_file.write(chunk)
+                dst_file.flush()
+    return file_name
+
+
+def download_all(root_url):
+    """ download all pdfs to local given the root_url
+    """
+    r = requests.get(root_url)
+    if r.status_code != 200:
+        return "Bad root url!"
+    soup = BeautifulSoup(r.text, 'lxml')
+    tags = soup.find_all('b')
+    # root_url is 'http://www.ieee-security.org/TC/SP2016/program-posters.html'
+    # home page is 'http://www.ieee-security.org/TC/SP2016/'
+    home = root_url[:-20] 
+    for i in range(len(tags)):
+        new_url = home + tags[i].contents[1]['href']
+        if new_url.endswith('.pdf'):
+            file_path = download_pdf(new_url)
+            print 'Downloading: ' + new_url + ' -> ' + file_path
+    print 'All downloading complete!'
+    return None
+
+#download_all(url3)
