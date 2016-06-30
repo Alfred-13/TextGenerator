589d0896b0029ef0e79f43879949f955f0bdf9cc
dcclogin <dccrumia@gmail.com>
2016-06-30 10:43:23 +0800

    updatecd TextGenerator/!

--- test_spider.py
+++ test_spider.py
@@ -17,3 +17,18 @@ def get_titles(url):
 
 #titles = get_titles(url)
 #print titles
+
+def get_intros(url):
+    """ return a dict containing all intros in one page
+    """
+    intr = dict()
+    html = urlopen(url)
+    # using lxml parser
+    soup = BeautifulSoup(html, 'lxml')
+    tags = soup.find_all('div', style='margin-left: 1.5em')
+    for i in range(len(tags)):
+        intr[i+1] = tags[i].contents[0].strip()
+    return intr
+
+#intros = get_intros(url)
+#print intros
